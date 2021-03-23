1ccfac3e53f17bbb49aaa02c53072cf49347114d
JI-doo <awt0311@gmail.com>
2021-03-23 15:51:00 +0900

    [ADD] auth.js

---
+++ src/middlewares/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+    const token = req.headers['access-token'];
+
+    if(!token) {
+        return res.status(403).json({
+            success: false,
+            message: 'token required'
+        });
+    }
+
+    try {
+        await jwt.verify(token, req.app.get("jwt-secret"), (err, decode) => {
+            if (err) rejects(err)
+            req.decode = decode;
+            next();
+        });
+    }catch {
+        res.status(401).json({ message: e.message});
+    }
+};
+
+module.exports = authMiddleware
+
