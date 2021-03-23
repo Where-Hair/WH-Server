6ab9352e2e25fe9162902ce3bd184c6561fd7345
JI-doo <awt0311@gmail.com>
2021-03-23 15:50:35 +0900

    [UPDATE] routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express')();
+const auth = require('./auth')();
 const user = require('./user')();
 const main = require('./main')();
 
+router.use("/auth", auth);
 router.use("/user", user);
 router.use("/main", main);
 
