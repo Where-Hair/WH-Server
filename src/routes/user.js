d62a46cfc7fd91ef7906aa11ad3f8a1695ad3232
JI-doo <awt0311@gmail.com>
2021-03-23 15:36:46 +0900

    [UPDATE] routes/user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,3 +2,7 @@ const router = require('express');
 const controller = require('../controller/user');
 const auth = require('../middlewares/auth');
 
+router.post('/signIn', controller.signIn);
+router.post('/signUp', controller.signUp);
+
+module.exports = router;
