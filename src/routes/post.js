4b54798749be7c6568941d900dc8c560d762fb28
JI-doo <awt0311@gmail.com>
2021-03-25 14:01:34 +0900

    [MODIFY] routes

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,5 +1,5 @@
 const router = require('express');
-const controller = require('../controller/post');
+const controller = require('../controller/post/post');
 
 router.get('/post', controller.post);
 router.get('/detailPost', controller.detailPost);
