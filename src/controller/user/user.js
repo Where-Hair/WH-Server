feabab178f5b3bfa669544ff177c3c8534b1a836
JI-doo <awt0311@gmail.com>
2021-03-25 09:42:26 +0900

    [ADD] register and login

---
+++ src/controller/user/user.js
@@ -0,0 +1,40 @@
+const { User } = require("../../model");
+const query = require("./query");
+const token = require("./token");
+
+const register = async (req, res) => {
+    const { nickname ,id, password } = req.body;
+    try {
+        const encodedPassword = await query.passwordEncoding(password);
+        await User.create({nickname, id, encodedPassword});
+        res.status(200).json({
+            message: "성공",
+        });  
+    } catch (err) {
+        res.status(409).json({
+            message: "이미 회원가입한 계정",
+            err: err.message
+        });
+    }
+};
+
+const login = async (req, res) => {
+    const { id, password } = req.body;
+    try {
+        const user = await query.findOneByUserId(id);
+        if(!user) throw new Error ("계정 없음");
+        if (!(await query.passwordCompare(password, user.password)))
+        res.status(409).end();
+        const accessToken = await access(user.id, process.env.SECRET_KEY);
+        res.status(200).json(accessToken);
+      } catch (err) {
+          res.status(404).json({
+              message: "실패",
+          });
+      } 
+};
+
+module.exports = {
+    register,
+    login,
+};
