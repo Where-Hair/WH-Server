cf422b121532287375b759c3065cb2216de2cd65
JI-doo <awt0311@gmail.com>
2021-04-02 16:13:07 +0900

    [ADD] profile controller

---
+++ src/controller/profile/profile.js
@@ -0,0 +1,21 @@
+const { Profile } = require("../../model");
+const query = require("./query");
+
+const showProfile = async (req, res) => {
+    const id = req.decode.id;
+    try {
+        const profile = await query(id);
+        res.status(200).json({
+            message: "성공"
+        });
+    } catch {
+        res.status(400).json({
+            message: "실패"
+        });
+    }
+};
+
+module.exports = {
+    showProfile,
+
+}
