0b362e7a16e699661c82664c4778205fa716d05a
JI-doo <awt0311@gmail.com>
2021-04-02 16:44:14 +0900

    [ADD] post controller

--- src/controller/post/post.js
+++ src/controller/post/post.js
@@ -1,16 +1,24 @@
 const { Post } = require("../../model");
 const query = require("./query");
 
-const showProfile = async (req, res) => {
-    const id = req.decode.id;
+const write = async (req, res, next) => {
+    const content  = req.body.content;
+    const id = req.decoded.id;
     try {
-        const profile = await query(id);
+        const post = await Post.findOne({ where: { id } });
+        if(!post) await Post.create({ content, id });
+        post.content = content;
+        await post.save();
         res.status(200).json({
-            message: "성공"
+            message: "성공",
         });
-    } catch {
-        res.status(400).json({
-            message: "실패"
+    } catch (err) {
+        res.status(404).json({
+            message: "실패",
         });
     }
+};
+
+module.exports = {
+    write,
 };
