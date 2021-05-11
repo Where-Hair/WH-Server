abdde127b2e1161c0773689776ca0cb30806da4f
JI-doo <awt0311@gmail.com>
2021-05-11 16:55:44 +0900

    [ADD] jwt and refresh secret

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ app.use((req, res, next) => {
     res.status(404).send('Not Found');
 });
 
+app.set("jwt-secret", process.env.JWT_SECRET);
+app.set("refresh-secret", process.env.REFRESH_SECRET);
+
 app.listen(3000, () => {
     console.log('Server listening at 3000 port')
 });
