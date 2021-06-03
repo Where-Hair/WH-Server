8686b03ea8b25393fc2152c09ad2f4fb979b7b85
JI-doo <awt0311@gmail.com>
2021-06-03 09:23:41 +0900

    [MODIFY] auth

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const authMiddleware = (req, res, next) => {
+const authMiddleware = async (req, res, next) => {
     const token = req.headers['access-token'];
 
     if(!token) {
